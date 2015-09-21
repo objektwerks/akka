0228f3755458d589a6771b7e1a28dc44f23811ad
objektwerks <objektwerks@gmail.com>
2015-09-21 15:07:43 -0400

    enhanced snapshot testing

--- src/test/scala/akka/PersistenceTest.scala
+++ src/test/scala/akka/PersistenceTest.scala
@@ -3,9 +3,9 @@ package akka
 import java.time.LocalDateTime
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{ActorLogging, ActorRef, ActorSystem, Props}
 import akka.pattern._
-import akka.persistence.{PersistentActor, SnapshotOffer}
+import akka.persistence.{SaveSnapshotSuccess, PersistentActor, SaveSnapshotFailure, SnapshotOffer}
 import akka.util.Timeout
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
@@ -30,7 +30,7 @@ case object State
 case object Snapshot
 case object Shutdown
 
-class Computer extends PersistentActor {
+class Computer extends PersistentActor with ActorLogging {
   override def persistenceId: String = "computer-persistence-id"
 
   var state = ComputedState()
@@ -47,6 +47,8 @@ class Computer extends PersistentActor {
       }
     case State => sender ! state.computedEvents.size
     case Snapshot => saveSnapshot(state)
+    case SaveSnapshotSuccess(metadata) => log.info(s"Computer snapshot successful: $metadata")
+    case SaveSnapshotFailure(metadata, reason) => throw reason
     case Shutdown => context.stop(self)
   }
 
@@ -58,7 +60,7 @@ class Computer extends PersistentActor {
 
 class PersistenceTest extends FunSuite with BeforeAndAfterAll {
   implicit val ec = ExecutionContext.global
-  implicit val timeout = new Timeout(3, TimeUnit.SECONDS)
+  implicit val timeout = new Timeout(1, TimeUnit.SECONDS)
   val system: ActorSystem = ActorSystem.create("persistence")
   val computer: ActorRef = system.actorOf(Props[Computer], name = "computer")
 
@@ -82,17 +84,17 @@ class PersistenceTest extends FunSuite with BeforeAndAfterAll {
     val event = ComputedEvent(command.execute)
     assert(event.value == 1)
 
-    for (n <- 1 to 10) {
+    for (n <- 1 to 100) {
       computer ! ComputeCommand(fibonacci, n)
+      if (n % 25 == 0) computer ! Snapshot
     }
-    computer ! Snapshot
 
-    val future = computer ? State
-    future onComplete {
-      case Success(state) => assert(state == 10)
+    val state = computer ? State
+    state onComplete {
+      case Success(count) => assert(count == 100)
       case Failure(failure) => throw failure
     }
-    Await.result(future, 1 second)
+    Await.result(state, 1 second)
 
     computer ! Shutdown
   }
