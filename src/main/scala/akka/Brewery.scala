3160837db2113feea5e4f63c997c1a71afad41da
objektwerks <objektwerks@gmail.com>
2015-09-25 19:27:24 -0400

    renamed classes

--- src/main/scala/akka/Brewery.scala
+++ src/main/scala/akka/Brewery.scala
@@ -1,8 +1,36 @@
 package akka
 
-import akka.actor.{Props, ActorRef, ActorSystem}
+import scalafx.application.JFXApp
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.control._
+import scalafx.scene.layout.VBox
 
-object Brewery extends App {
-  val system: ActorSystem = ActorSystem.create("IPAFactory")
-  val brewer: ActorRef = system.actorOf(Props[Brewer], name = "brewer")
+object Brewery extends JFXApp {
+  val contentPane = new VBox {
+    maxWidth = 400
+    maxHeight = 400
+    spacing = 6
+    padding = Insets(6)
+    children = List()
+  }
+
+  val toolbar = new ToolBar {
+    content = List()
+  }
+
+  val appPane = new VBox {
+    maxWidth = 400
+    maxHeight = 400
+    spacing = 6
+    padding = Insets(6)
+    children = List(toolbar, contentPane)
+  }
+
+  stage = new JFXApp.PrimaryStage {
+    title.value = "IPA Factory"
+    scene = new Scene {
+      root = appPane
+    }
+  }
 }
