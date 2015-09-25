852216dc85522ae9d64d32be18bebecb25f84038
objektwerks <objektwerks@gmail.com>
2015-09-25 17:45:45 -0400

    added process and step actor init code

--- src/main/scala/akka/Cluster.scala
+++ src/main/scala/akka/Cluster.scala
@@ -1,7 +1,9 @@
 package akka
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, ActorRef, ActorSystem}
 
 object Cluster extends App {
   val system: ActorSystem = ActorSystem.create("IPAFactory")
+  val process: ActorRef = system.actorOf(Props[Process], name = "process")
+  val step: ActorRef = system.actorOf(Props[Step], name = "step")
 }
