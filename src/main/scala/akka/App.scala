8a95d59919d76476d86a0c178e5bddd4c579f60b
objektwerks <objektwerks@gmail.com>
2015-09-25 16:44:54 -0400

    skeleton scalafx app for akka cluster

---
+++ src/main/scala/akka/App.scala
@@ -0,0 +1,36 @@
+package akka
+
+import scalafx.application.JFXApp
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.control._
+import scalafx.scene.layout.VBox
+
+object App extends JFXApp {
+  val contentPane = new VBox {
+    maxWidth = 400
+    maxHeight = 400
+    spacing = 6
+    padding = Insets(6)
+    children = List()
+  }
+
+  val toolbar = new ToolBar {
+    content = List()
+  }
+
+  val appPane = new VBox {
+    maxWidth = 400
+    maxHeight = 400
+    spacing = 6
+    padding = Insets(6)
+    children = List(toolbar, contentPane)
+  }
+
+  stage = new JFXApp.PrimaryStage {
+    title.value = "IPA Factory"
+    scene = new Scene {
+      root = appPane
+    }
+  }
+}
