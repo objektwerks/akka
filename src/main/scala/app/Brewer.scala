b0fd1285b12b3fec1c468f3d4d1a6c7abaa3b43c
objektwerks <objektwerks@gmail.com>
2015-09-28 11:25:50 -0400

    refactored mistake ;)

--- src/main/scala/app/Brewer.scala
+++ src/main/scala/app/Brewer.scala
@@ -2,10 +2,10 @@ package app
 
 import akka.actor.Actor
 
-case class Brew(recipe: Recipe)
+case class Brew(batch: Batch)
 
 case class Brewer() extends Actor {
   override def receive: Receive = {
-    case Brew(recipe) => recipe.brew()
+    case Brew(batch) => batch.brew()
   }
 }
