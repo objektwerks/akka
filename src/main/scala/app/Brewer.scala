08f611639b5e7fb467c6b675fbcc2bfc682e0458
objektwerks <objektwerks@gmail.com>
2015-09-28 11:30:40 -0400

    brewer no long a case class as an actor

--- src/main/scala/app/Brewer.scala
+++ src/main/scala/app/Brewer.scala
@@ -4,7 +4,7 @@ import akka.actor.Actor
 
 case class Brew(batch: Batch)
 
-case class Brewer() extends Actor {
+class Brewer() extends Actor {
   override def receive: Receive = {
     case Brew(batch) => batch.brew()
   }
