e92f02016e1598f3c23eddb51feadaaa7321eb36
objektwerks <objektwerks@gmail.com>
2015-09-29 12:17:08 -0400

    removed private modifiers

--- src/main/scala/app/Brewery.scala
+++ src/main/scala/app/Brewery.scala
@@ -5,9 +5,9 @@ import java.time.LocalDateTime
 import akka.actor._
 
 class Brewery(batchEventListener: BatchEventListener) {
-  private val system: ActorSystem = ActorSystem.create("Brewery")
-  private val brewer: ActorRef = system.actorOf(Props[Brewer], name = "brewer")
-  private val listener: ActorRef = system.actorOf(Props(new BatchListener(batchEventListener)), name = "listener")
+  val system: ActorSystem = ActorSystem.create("Brewery")
+  val brewer: ActorRef = system.actorOf(Props[Brewer], name = "brewer")
+  val listener: ActorRef = system.actorOf(Props(new BatchListener(batchEventListener)), name = "listener")
   system.eventStream.subscribe(listener, classOf[Batch])
 
   def brew(recipe: Recipe): Unit = {
