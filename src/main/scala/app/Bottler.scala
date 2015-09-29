67149e2a716f22ac7f1bc0839e789e094547654e
objektwerks <objektwerks@gmail.com>
2015-09-29 13:13:15 -0400

    adjust completed batch date time

--- src/main/scala/app/Bottler.scala
+++ src/main/scala/app/Bottler.scala
@@ -1,9 +1,11 @@
 package app
 
+import java.time.LocalDateTime
+
 import akka.actor.Actor
 
 class Bottler extends Actor {
   override def receive: Receive = {
-    case batch: Batch => context.system.eventStream.publish(batch)
+    case batch: Batch => context.system.eventStream.publish(batch.completed.adjustInto(LocalDateTime.now))
   }
 }
