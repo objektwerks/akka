b85c37766ee530c1926193cee8f8bea7ef9ce90c
objektwerks <objektwerks@gmail.com>
2015-09-29 08:41:25 -0400

    refactoring once again

---
+++ src/main/scala/app/Brews.scala
@@ -0,0 +1,21 @@
+package app
+
+case class Malt(kind: String)
+
+case class Hop(kind: String)
+
+case class Hops(kinds: Array[Hop])
+
+case class Yeast(kind: String)
+
+case class Water(kind: String)
+
+trait Recipe {
+  def name: String
+  def description: String
+  def malt: Malt
+  def hops: Hops
+  def yeast: Yeast
+  def water: Water
+}
+
