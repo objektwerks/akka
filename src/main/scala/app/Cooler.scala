bbb3f2d812c745de74f7f78c73e58ec90e2a81b7
objektwerks <objektwerks@gmail.com>
2015-09-29 15:36:42 -0400

    implemented akka cluster boilerplate code for each actor

--- src/main/scala/app/Cooler.scala
+++ src/main/scala/app/Cooler.scala
@@ -1,9 +1,23 @@
 package app
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{ActorLogging, Actor, ActorRef}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
 
-class Cooler(fermenter: ActorRef) extends Actor {
+class Cooler(fermenter: ActorRef) extends Actor with ActorLogging {
+  val cluster = Cluster(context.system)
+
+  override def preStart(): Unit = {
+    cluster.subscribe(self, initialStateMode = InitialStateAsEvents, classOf[MemberEvent], classOf[UnreachableMember])
+  }
+
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
   override def receive: Receive = {
     case batch: Batch => fermenter ! batch
+    case MemberUp(member) => log.info("Member is Up: {}", member.address)
+    case UnreachableMember(member) => log.warning("Member detected as unreachable: {}", member)
+    case MemberRemoved(member, previousStatus) => log.info("Member is Removed: {} after {}", member.address, previousStatus)
+    case _: MemberEvent => log.warning("Unknown Member event!")
   }
 }
--- src/main/scala/app/Masher.scala
+++ src/main/scala/app/Masher.scala
@@ -1,9 +1,23 @@
 package app
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{ActorLogging, Actor, ActorRef}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
 
-class Masher(boiler: ActorRef) extends Actor {
+class Masher(boiler: ActorRef) extends Actor with ActorLogging {
+  val cluster = Cluster(context.system)
+
+  override def preStart(): Unit = {
+    cluster.subscribe(self, initialStateMode = InitialStateAsEvents, classOf[MemberEvent], classOf[UnreachableMember])
+  }
+
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
   override def receive: Receive = {
     case batch: Batch => boiler ! batch
+    case MemberUp(member) => log.info("Member is Up: {}", member.address)
+    case UnreachableMember(member) => log.warning("Member detected as unreachable: {}", member)
+    case MemberRemoved(member, previousStatus) => log.info("Member is Removed: {} after {}", member.address, previousStatus)
+    case _: MemberEvent => log.warning("Unknown Member event!")
   }
 }
