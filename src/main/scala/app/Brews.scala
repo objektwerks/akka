ba662dd9ffa36bb168ddb3ff2ea59f70aaad8535
objektwerks <objektwerks@gmail.com>
2015-09-29 11:17:13 -0400

    added default IPA receipe

--- src/main/scala/app/Brews.scala
+++ src/main/scala/app/Brews.scala
@@ -4,8 +4,6 @@ case class Malt(kind: String)
 
 case class Hop(kind: String)
 
-case class Hops(kinds: Array[Hop])
-
 case class Yeast(kind: String)
 
 case class Water(kind: String)
@@ -14,8 +12,14 @@ trait Recipe {
   def name: String
   def description: String
   def malt: Malt
-  def hops: Hops
+  def hops: List[Hop]
   def yeast: Yeast
   def water: Water
 }
 
+case class IPA(name: String = "IPA",
+               description: String = "Indian Pale Ale",
+               malt: Malt = Malt("Caramel"),
+               hops: List[Hop] = List(Hop("Cascade"), Hop("Amarillo")),
+               yeast: Yeast = Yeast("Brewers"),
+               water: Water = Water("Spring")) extends Recipe
