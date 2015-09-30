17dad73b26e33e1ac59a068caca8d21b9deacc70
objektwerks <objektwerks@gmail.com>
2015-09-30 07:56:24 -0400

    removed logging

--- src/test/scala/akka/BreweryTest.scala
+++ src/test/scala/akka/BreweryTest.scala
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit
 import akka.util.Timeout
 import app.{Batch, BatchEventListener, Brewery, IPA}
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
-import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future, Await}
 import scala.concurrent.duration._
@@ -13,11 +12,10 @@ import scala.concurrent.duration._
 class BreweryTest extends FunSuite with BeforeAndAfterAll {
   implicit val ec = ExecutionContext.global
   implicit val timeout = new Timeout(1, TimeUnit.SECONDS)
-  val log = LoggerFactory.getLogger(this.getClass)
   val listener = new BatchEventListener() {
     override def onEvent(batch: Batch): Unit = {
       assert(batch != null)
-      log.info(s"*** Batch received: $batch")
+      println(s"*** Batch received: $batch")
     }
   }
   val brewery = new Brewery(listener)
@@ -28,7 +26,6 @@ class BreweryTest extends FunSuite with BeforeAndAfterAll {
 
   test("brew") {
     brewery.brew(IPA())
-    log.info("*** Brewing...")
     Await.result(Future { Thread.sleep(2000) }, 3 seconds)
   }
 }
