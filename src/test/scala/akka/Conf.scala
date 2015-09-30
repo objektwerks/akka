eaf9374a6b5e66be461fec488aa585205b05b089
objektwerks <objektwerks@gmail.com>
2015-09-30 08:01:53 -0400

    added general testing logger to Conf

--- src/test/scala/akka/Conf.scala
+++ src/test/scala/akka/Conf.scala
@@ -1,7 +1,9 @@
 package akka
 
 import com.typesafe.config.{Config, ConfigFactory}
+import org.slf4j.{LoggerFactory, Logger}
 
 object Conf {
   val config: Config = ConfigFactory.load("test.conf")
+  val log: Logger = LoggerFactory.getLogger(this.getClass)
 }
