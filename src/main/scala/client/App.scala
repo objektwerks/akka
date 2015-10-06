d8d028bce80f62680ae1d064e1034b2cae7b939d
objektwerks <objektwerks@gmail.com>
2015-10-06 09:15:46 -0400

    enhanced logging and ui

--- src/main/scala/client/App.scala
+++ src/main/scala/client/App.scala
@@ -4,7 +4,7 @@ import domain.{IPA, Recipe}
 import event.Brewed
 
 import scalafx.Includes._
-import scalafx.application.JFXApp
+import scalafx.application.{Platform, JFXApp}
 import scalafx.beans.property.ObjectProperty
 import scalafx.event.ActionEvent
 import scalafx.geometry.Insets
@@ -29,19 +29,23 @@ object App extends JFXApp {
     content = List(brewButton, new Separator(), statusBar)
   }
 
-  val statusText = new Text
+  val recipeLabel = new Label
+  val recipeText = new Text
+
+  val brewedLabel = new Label
+  val brewedText = new Text
 
   val contentPane = new VBox {
-    maxWidth = 400
-    maxHeight = 400
+    maxWidth = 600
+    maxHeight = 600
     spacing = 6
     padding = Insets(6)
-    children = List(statusText)
+    children = List(recipeLabel, recipeText, brewedLabel, brewedText)
   }
 
   val appPane = new VBox {
-    maxWidth = 400
-    maxHeight = 400
+    maxWidth = 600
+    maxHeight = 600
     spacing = 6
     padding = Insets(6)
     children = List(toolbar, contentPane)
@@ -55,12 +59,16 @@ object App extends JFXApp {
   }
 
   recipeProperty.onChange({
-    statusBar.text = "Brewing..."
-    statusText.text = recipeProperty.value.toString
+    Platform.runLater({
+      statusBar.text = "Brewing..."
+      recipeText.text = recipeProperty.value.toString
+    })
   })
 
   brewedProperty.onChange({
-    statusBar.text = "Brewed!"
-    statusText.text = brewedProperty.value.toString
+    Platform.runLater({
+      statusBar.text = "Brewed!"
+      brewedText.text = brewedProperty.value.toString
+    })
   })
 }
--- src/main/scala/client/BreweryProxy.scala
+++ src/main/scala/client/BreweryProxy.scala
@@ -6,15 +6,18 @@ import akka.cluster.pubsub.{DistributedPubSub, DistributedPubSubMediator}
 import com.typesafe.config.ConfigFactory
 import domain.Recipe
 import event.Brewed
+import org.slf4j.{LoggerFactory, Logger}
 
 import scalafx.beans.property.ObjectProperty
 
-class BreweryPublisher extends Actor {
+class BreweryPublisher extends Actor with ActorLogging {
   import DistributedPubSubMediator.Publish
   val mediator = DistributedPubSub(context.system).mediator
 
   override def receive: Receive = {
-    case recipe: Recipe => mediator ! Publish(topic = "recipe", recipe)
+    case recipe: Recipe =>
+      log.info(s"Brewery Publisher recipe: $recipe")
+      mediator ! Publish(topic = "recipe", recipe)
   }
 }
 
@@ -24,17 +27,21 @@ class BrewerySubscriber extends Actor with ActorLogging {
   mediator ! Subscribe(topic = "brewed", self)
 
   override def receive: Receive = {
-    case brewed: Brewed => BreweryProxy.brewed(brewed)
+    case brewed: Brewed =>
+      log.info(s"Brewery Subscriber brewed: $brewed")
+      BreweryProxy.brewed(brewed)
     case SubscribeAck(Subscribe("brewed", None, `self`)) => log.info("Brewery Subscriber subscribed to brewed topic.")
   }
 }
 
 object BreweryProxy {
+  val log = LoggerFactory.getLogger(this.getClass)
   val system = ActorSystem.create("Brewery", ConfigFactory.load("app.conf"))
   val publisher: ActorRef = system.actorOf(Props[BreweryPublisher], name = "brewery.client.publisher")
   val subscriber: ActorRef = system.actorOf(Props[BrewerySubscriber], name = "brewery.client.subscriber")
   var recipePropertyListener: Option[ObjectProperty[Recipe]] = None
   var brewedPropertyListener: Option[ObjectProperty[Brewed]] = None
+  log.info("Brewery Proxy initialized!")
 
   def register(recipeProperty: ObjectProperty[Recipe], brewedProperty: ObjectProperty[Brewed]): Unit = {
     recipePropertyListener = Some(recipeProperty)
