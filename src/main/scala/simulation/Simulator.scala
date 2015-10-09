405d3180dc5a22540797be2ad64926fcae4b6291
objektwerks <objektwerks@gmail.com>
2015-10-09 17:06:27 -0400

    added fibonacci simulator

---
+++ src/main/scala/simulation/Simulator.scala
@@ -0,0 +1,18 @@
+package simulation
+
+import scala.annotation.tailrec
+
+class Simulator {
+  private[this] def tailrec(n: Long): BigInt = {
+    @tailrec
+    def loop(n: Long, a: Long, b: Long): BigInt = n match {
+      case 0 => a
+      case _ => loop(n - 1, b, a + b)
+    }
+    loop(n, 0, 1)
+  }
+
+  def simulate(number: Int): BigInt = {
+    tailrec(number)
+  }
+}
