af998a9b491d66f1b1ebdda4d14f3890ba0931b7
objektwerks <objektwerks@gmail.com>
2015-10-10 10:32:15 -0400

    added brew actor ref to assistant

--- src/main/scala/actor/Assistant.scala
+++ src/main/scala/actor/Assistant.scala
@@ -5,7 +5,7 @@ import system.Brewery
 import command.Brew
 import event.Stage
 
-class Assistant(masher: ActorRef) extends Actor with ActorLogging {
+class Assistant(brewer: ActorRef, masher: ActorRef) extends Actor with ActorLogging {
   override def receive: Receive = {
     case brew: Brew => masher ! brew
     case stage: Stage => Brewery.stage(stage)
