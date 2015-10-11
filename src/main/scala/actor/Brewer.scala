44a1b998f47e38c9153b38b62ceda0c3a86b2204
objektwerks <objektwerks@gmail.com>
2015-10-11 06:44:37 -0400

    added assistant router to brewer

--- src/main/scala/actor/Brewer.scala
+++ src/main/scala/actor/Brewer.scala
@@ -3,16 +3,25 @@ package actor
 import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicInteger
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.cluster.ClusterEvent._
 import akka.cluster.{Cluster, MemberStatus}
-import system.Brewery
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
 import command.Brew
 import domain.Recipe
 import event.{Brewed, Stage}
+import system.Brewery
 
 class Brewer(masher: ActorRef) extends Actor with ActorLogging {
   val batchNumber = new AtomicInteger()
+  var router = {
+    val routees = Vector.fill(3) {
+      val assistant = context.actorOf(Props[Assistant])
+      context watch assistant
+      ActorRefRoutee(assistant)
+    }
+    Router(RoundRobinRoutingLogic(), routees)
+  }
 
   override def preStart(): Unit = {
     Cluster(context.system).subscribe(self, classOf[ClusterDomainEvent])
@@ -24,7 +33,7 @@ class Brewer(masher: ActorRef) extends Actor with ActorLogging {
   }
 
   override def receive: Receive = {
-    case recipe: Recipe => masher ! Brew(batchNumber.incrementAndGet(), LocalDateTime.now, recipe)
+    case recipe: Recipe => masher ! Brew(batchNumber.incrementAndGet(), LocalDateTime.now, recipe) // router.route()
     case stage: Stage => Brewery.stage(stage)
     case brewed: Brewed => Brewery.brewed(brewed)
 
