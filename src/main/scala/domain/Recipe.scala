7034a3044430cfc9fc1e087d6c970100d5552535
objektwerks <objektwerks@gmail.com>
2015-10-13 16:13:39 -0400

    enhanced recipe; and added DIPA

--- src/main/scala/domain/Recipe.scala
+++ src/main/scala/domain/Recipe.scala
@@ -1,16 +1,15 @@
 package domain
 
-case class Malt(kind: String)
+case class Malt(kind: String, amount: String)
+case class Hop(kind: String, amount: String)
+case class Yeast(kind: String, amount: String)
+case class Water(kind: String, amount: String)
 
-case class Hop(kind: String)
-
-case class Yeast(kind: String)
-
-case class Water(kind: String)
-
-trait Recipe {
+sealed trait Recipe {
   def name: String
   def description: String
+  def ibu: Int
+  def abv: Double
   def malt: Malt
   def hops: List[Hop]
   def yeast: Yeast
@@ -19,7 +18,18 @@ trait Recipe {
 
 case class IPA(name: String = "IPA",
                description: String = "Indian Pale Ale",
-               malt: Malt = Malt("Caramel"),
-               hops: List[Hop] = List(Hop("Cascade"), Hop("Amarillo")),
-               yeast: Yeast = Yeast("Brewers"),
-               water: Water = Water("Spring")) extends Recipe
+               ibu: Int = 60,
+               abv: Double = 5.8,
+               malt: Malt = Malt("Pale", "12lb"),
+               hops: List[Hop] = List(Hop("Simcoe", ".5oz"), Hop("Amarillo", ".5oz"), Hop("Palisade", ".5oz"), Hop("Glacier", ".5oz")),
+               yeast: Yeast = Yeast("English Ale", "1oz"),
+               water: Water = Water("Spring", "5g")) extends Recipe
+
+case class DIPA(name: String = "DIPA",
+                description: String = "Double Indian Pale Ale",
+                ibu: Int = 75,
+                abv: Double = 9.0,
+                malt: Malt = Malt("Pale", "12lb"),
+                hops: List[Hop] = List(Hop("Simcoe", ".5oz"), Hop("Amarillo", ".5oz"), Hop("Palisade", ".5oz"), Hop("Glacier", ".5oz")),
+                yeast: Yeast = Yeast("English Ale", "1oz"),
+                water: Water = Water("Spring", "5g")) extends Recipe
