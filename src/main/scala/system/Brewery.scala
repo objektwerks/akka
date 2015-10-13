378bdfd3279df54be1cb08db4f2206481a4c9df2
objektwerks <objektwerks@gmail.com>
2015-10-13 11:54:00 -0400

    extracted listener from brewery

--- src/main/scala/system/Brewery.scala
+++ src/main/scala/system/Brewery.scala
@@ -4,8 +4,6 @@ import java.util.concurrent.TimeUnit
 
 import actor._
 import akka.actor._
-import akka.cluster.ClusterEvent._
-import akka.cluster.{Cluster, MemberStatus}
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import command.Command
@@ -23,7 +21,6 @@ object Brewery {
   val log = LoggerFactory.getLogger(this.getClass)
   var commandPropertyListener: Option[ObjectProperty[Command]] = None
   var eventPropertyListener: Option[ObjectProperty[Event]] = None
-
   val system = ActorSystem.create("Brewery", ConfigFactory.load("brewery.conf"))
   val bottler: ActorRef = system.actorOf(Props[Bottler], name = "bottler")
   val conditioner: ActorRef = system.actorOf(Props(new Conditioner(bottler)), name = "conditioner")
@@ -35,7 +32,6 @@ object Brewery {
   system.eventStream.subscribe(brewer, classOf[Command])
   system.eventStream.subscribe(brewer, classOf[Event])
   system.actorOf(Props[Listener], name = "listener")
-
   log.info("Brewery initialized!")
 
   def register(commandProperty: ObjectProperty[Command], eventProperty: ObjectProperty[Event]): Unit = {
@@ -58,27 +54,5 @@ object Brewery {
   def shutdown(): Unit = {
     Await.result(system.terminate(), 3 seconds)
     log.info("Brewery shutdown!")
-  }
-}
-
-class Listener extends Actor with ActorLogging {
-  override def preStart(): Unit = {
-    Cluster(context.system).subscribe(self, classOf[ClusterDomainEvent])
-  }
-
-  override def postStop(): Unit = {
-    Cluster(context.system).unsubscribe(self)
-    super.postStop()
-  }
-
-  override def receive: Receive = {
-    case MemberUp(member) => log.info(s"$member UP.")
-    case MemberExited(member) => log.info(s"$member EXITED.")
-    case MemberRemoved(member, previousState) =>
-      if(previousState == MemberStatus.Exiting) log.info(s"$member previously gracefully EXITED, REMOVED.")
-      else log.info(s"$member previously downed after UNREACHABLE, REMOVED.")
-    case UnreachableMember(member) => log.info(s"$member UNREACHABLE")
-    case ReachableMember(member) => log.info(s"$member REACHABLE")
-    case state: CurrentClusterState => log.info(s"CURRENT CLUSTER STATE: $state")
   }
 }
---
+++ src/main/scala/system/Listener.scala
@@ -0,0 +1,28 @@
+package system
+
+import akka.actor.{ActorLogging, Actor}
+import akka.cluster.ClusterEvent._
+import akka.cluster.{Cluster, MemberStatus}
+
+
+class Listener extends Actor with ActorLogging {
+  override def preStart(): Unit = {
+    Cluster(context.system).subscribe(self, classOf[ClusterDomainEvent])
+  }
+
+  override def postStop(): Unit = {
+    Cluster(context.system).unsubscribe(self)
+    super.postStop()
+  }
+
+  override def receive: Receive = {
+    case MemberUp(member) => log.info(s"$member UP.")
+    case MemberExited(member) => log.info(s"$member EXITED.")
+    case MemberRemoved(member, previousState) =>
+      if(previousState == MemberStatus.Exiting) log.info(s"$member previously gracefully EXITED, REMOVED.")
+      else log.info(s"$member previously downed after UNREACHABLE, REMOVED.")
+    case UnreachableMember(member) => log.info(s"$member UNREACHABLE")
+    case ReachableMember(member) => log.info(s"$member REACHABLE")
+    case state: CurrentClusterState => log.info(s"CURRENT CLUSTER STATE: $state")
+  }
+}
