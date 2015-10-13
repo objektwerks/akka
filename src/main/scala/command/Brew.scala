7150901ec06f41a0faaf2e694ed879286343bb95
objektwerks <objektwerks@gmail.com>
2015-10-13 11:37:29 -0400

    reformatted code

--- src/main/scala/command/Brew.scala
+++ src/main/scala/command/Brew.scala
@@ -4,6 +4,4 @@ import java.time.LocalTime
 
 import domain.Recipe
 
-case class Brew(number: Int,
-                initiated: LocalTime,
-                recipe: Recipe) extends Command
+case class Brew(number: Int, initiated: LocalTime, recipe: Recipe) extends Command
