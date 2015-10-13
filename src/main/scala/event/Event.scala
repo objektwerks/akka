7e23eaea6585ec59be5dd954fcc6e36a5baddf8d
objektwerks <objektwerks@gmail.com>
2015-10-13 17:57:43 -0400

    refactored event

--- src/main/scala/event/Event.scala
+++ src/main/scala/event/Event.scala
@@ -11,16 +11,16 @@ case class Brewing(batch: Int, completed: LocalTime) extends Event
 case class Brewed(batch: Int, initiated: LocalTime, completed: LocalTime) extends Event
 
 case class Casking(batch: Int, completed: LocalTime) extends Event
-case class Casked(number: Int, completed: LocalTime) extends Event
+case class Casked(batch: Int, completed: LocalTime) extends Event
 
 case class Conditioning(batch: Int, completed: LocalTime) extends Event
-case class Conditioned(number: Int, completed: LocalTime) extends Event
+case class Conditioned(batch: Int, completed: LocalTime) extends Event
 
 case class Cooling(batch: Int, completed: LocalTime) extends Event
-case class Cooled(number: Int, completed: LocalTime) extends Event
+case class Cooled(batch: Int, completed: LocalTime) extends Event
 
 case class Fermenting(batch: Int, completed: LocalTime) extends Event
-case class Fermented(number: Int, completed: LocalTime) extends Event
+case class Fermented(batch: Int, completed: LocalTime) extends Event
 
 case class Mashing(batch: Int, completed: LocalTime) extends Event
-case class Mashed(number: Int, completed: LocalTime) extends Event
+case class Mashed(batch: Int, completed: LocalTime) extends Event
