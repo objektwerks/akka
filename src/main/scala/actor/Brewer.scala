a1cdaee971e27d135f4ac8b4bc7de3f0f4a19279
objektwerks <objektwerks@gmail.com>
2015-11-06 13:26:55 -0500

    refactored brewery actor creation

--- src/main/scala/actor/Brewer.scala
+++ src/main/scala/actor/Brewer.scala
@@ -2,22 +2,13 @@ package actor
 
 import java.util.concurrent.atomic.AtomicInteger
 
-import akka.actor.{Props, Actor, ActorRef}
+import akka.actor.{ActorRef, Actor}
 import command.{Brew, Command}
 import domain.Recipe
 import event.{Brewing, Event}
 import system.Brewery
 
-class Brewer() extends Actor {
-  val system = context.system
-  val bottler: ActorRef = system.actorOf(Props[Bottler], name = "bottler")
-  val kegger: ActorRef = system.actorOf(Props[Kegger], name = "kegger")
-  val casker: ActorRef = system.actorOf(Props[Casker], name = "casker")
-  val conditioner: ActorRef = system.actorOf(Props(new Conditioner(bottler, kegger, casker)), name = "conditioner")
-  val fermenter: ActorRef = system.actorOf(Props(new Fermenter(conditioner)), name = "fermenter")
-  val cooler: ActorRef = system.actorOf(Props(new Cooler(fermenter)), name = "cooler")
-  val boiler: ActorRef = system.actorOf(Props(new Boiler(cooler)), name = "boiler")
-  val masher: ActorRef = system.actorOf(Props(new Masher(boiler)), name = "masher")
+class Brewer(masher: ActorRef) extends Actor {
   val batchNumber = new AtomicInteger()
   val publisher = context.system.eventStream
 
