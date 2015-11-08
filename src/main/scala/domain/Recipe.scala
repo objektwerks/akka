ec99ec6cdbc4e785882156307ec2d68c358b90bd
objektwerks <objektwerks@gmail.com>
2015-11-08 07:50:08 -0500

    refactored recipe for better type safety

--- src/main/scala/domain/Recipe.scala
+++ src/main/scala/domain/Recipe.scala
@@ -1,14 +1,18 @@
 package domain
 
-case class Ingrediant(kind: String, amount: String)
-case class Malt(kind: String, amount: String)
-case class Hop(kind: String, amount: String)
-case class Yeast(kind: String, amount: String)
-case class Water(kind: String, amount: String, boilSize: String, boilTime: String, batchSize: String)
-case class Fermentation(kind: String, days: Int, degrees: Int)
+case class Ingrediant(kind: String, amount: Double, measurement: Measurement.Value)
+case class Malt(kind: String, amount: Double, measurement: Measurement.Value)
+case class Hop(kind: String, oz: Double)
+case class Yeast(kind: String, oz: Double)
+case class Water(gallons: Double, boilSizeInGallons: Double, boildTimeInMinutes: Int, batchSizeInGallons: Double)
+case class Fermentation(days: Int, degrees: Int)
 object Phase extends Enumeration {
   type Phase = Value
   val Masher, Boiler, Cooler, Fermenter, Conditioner = Value
+}
+object Measurement extends Enumeration {
+  type Measurement = Value
+  val lb, oz, tsp = Value
 }
 
 sealed trait Recipe {
@@ -24,9 +28,13 @@ sealed trait Recipe {
   def hops: List[Hop]
   def yeast: Yeast
   def water: Water
-  def fermentations: List[Fermentation]
+  def primary: Fermentation
+  def secondary: Fermentation
   def instructions: Map[Phase.Value, List[String]]
 }
+
+import Phase._
+import Measurement._
 
 case class IPA(name: String = "Dogfish Head 60' IPA",
                style: String = "IPA",
@@ -35,18 +43,21 @@ case class IPA(name: String = "Dogfish Head 60' IPA",
                originalGravity: Double = 1.070,
                finalGravity: Double = 1.018,
                color: Double = 4.8,
-               ingrediants: List[Ingrediant] = List(Ingrediant("Irish Moss", "1 tsp"), Ingrediant("Corn Sugar", "4 oz")),
-               malts: List[Malt] = List(Malt("2 Row Pale", "13 lb"), Malt("Thomas Fawcett Amber Malt", "6 oz")),
-               hops: List[Hop] = List(Hop("Simcoe", ".5 oz"),
-                                      Hop("Amarillo", "1.0 oz"),
-                                      Hop("Palisade", ".5 oz"),
-                                      Hop("Glacier", ".5 oz"),
-                                      Hop("Warrior", ".75 oz")),
-               yeast: Yeast = Yeast("English Ale", "1 oz"),
-               water: Water = Water("Spring", "5 g", "6 g", "60 m", "5 g"),
-               fermentations: List[Fermentation] = List(Fermentation("primary", 10, 63), Fermentation("secondary", 10, 63)),
-               instructions: Map[Phase.Value, List[String]] = Map(Phase.Masher -> List("Mash."),
-                                                                  Phase.Boiler -> List("Boil."),
-                                                                  Phase.Cooler -> List("Cool."),
-                                                                  Phase.Fermenter -> List("Ferment."),
-                                                                  Phase.Conditioner -> List("Condition."))) extends Recipe
+               ingrediants: List[Ingrediant] = List(Ingrediant("Irish Moss", 1.0, tsp),
+                                                    Ingrediant("Corn Sugar", 4.0, oz)),
+               malts: List[Malt] = List(Malt("2 Row Pale", 13.0, lb),
+                                        Malt("Thomas Fawcett Amber Malt", 6.0, oz)),
+               hops: List[Hop] = List(Hop("Simcoe", 0.5),
+                                      Hop("Amarillo", 1.0),
+                                      Hop("Palisade", 0.5),
+                                      Hop("Glacier", 0.5),
+                                      Hop("Warrior", 0.75)),
+               yeast: Yeast = Yeast("English Ale", 1.0),
+               water: Water = Water(5.0, 6.0, 60, 5.0),
+               primary: Fermentation = Fermentation(10, 63),
+               secondary: Fermentation = Fermentation(10, 63),
+               instructions: Map[Phase.Value, List[String]] = Map(Masher -> List("Mash."),
+                                                                  Boiler -> List("Boil."),
+                                                                  Cooler -> List("Cool."),
+                                                                  Fermenter -> List("Ferment."),
+                                                                  Conditioner -> List("Condition."))) extends Recipe
