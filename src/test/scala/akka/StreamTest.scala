c18dd951b69e0ae0ce7593c0f34e3509cd2b34f0
objektwerks <objektwerks@gmail.com>
2016-02-22 13:56:27 -0500

    added stream test

---
+++ src/test/scala/akka/StreamTest.scala
@@ -0,0 +1,16 @@
+package akka
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import org.scalatest.FunSuite
+
+class StreamTest extends FunSuite {
+  implicit val system: ActorSystem = ActorSystem.create("stream", Conf.config)
+  implicit val materializer = ActorMaterializer()
+
+  test("simple") {
+    val source: Source[Int, NotUsed] = Source(1 to 10)
+    source.runForeach(i => assert(i > 0 && i < 11))(materializer)
+  }
+}
