55d7537ba93232d07cffabed0d47c9a76758c10e
objektwerks <objektwerks@gmail.com>
2016-02-24 11:24:27 -0500

    added run with test

--- src/test/scala/akka/StreamTest.scala
+++ src/test/scala/akka/StreamTest.scala
@@ -2,7 +2,7 @@ package akka
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Sink, Source}
 import org.scalatest.FunSuite
 
 import scala.concurrent.ExecutionContext
@@ -12,20 +12,26 @@ class StreamTest extends FunSuite {
   implicit val materializer = ActorMaterializer()
   implicit val ec = ExecutionContext.global
 
-  test("runForeach") {
+  test("run foreach") {
     val source: Source[Int, NotUsed] = Source(1 to 10)
     source.runForeach(i => assert(i > 0 && i < 11))
   }
 
-  test("runFold") {
+  test("run fold") {
     val source: Source[Int, NotUsed] = Source(1 to 10)
     val result = source.runFold(0)(_ + _)
     result map { r => assert(r == 10) }
   }
 
-  test("runReduce") {
+  test("run reduce") {
     val source: Source[Int, NotUsed] = Source(1 to 10)
     val result = source.runReduce(_ + _)
+    result map { r => assert(r == 10) }
+  }
+
+  test("run with") {
+    val source: Source[Int, NotUsed] = Source(1 to 10)
+    val result = source.runWith(Sink.fold(0)(_ + _))
     result map { r => assert(r == 10) }
   }
 }
