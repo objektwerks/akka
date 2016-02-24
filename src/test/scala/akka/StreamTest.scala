78b6a1fecd83e1310decf3714907e8e28ff83f23
objektwerks <objektwerks@gmail.com>
2016-02-24 11:08:33 -0500

    added run fold test

--- src/test/scala/akka/StreamTest.scala
+++ src/test/scala/akka/StreamTest.scala
@@ -5,12 +5,21 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 import org.scalatest.FunSuite
 
+import scala.concurrent.ExecutionContext
+
 class StreamTest extends FunSuite {
   implicit val system: ActorSystem = ActorSystem.create("stream", Conf.config)
   implicit val materializer = ActorMaterializer()
+  implicit val ec = ExecutionContext.global
 
-  test("source") {
+  test("runForeach") {
     val source: Source[Int, NotUsed] = Source(1 to 10)
     source.runForeach(i => assert(i > 0 && i < 11))
+  }
+
+  test("runFold") {
+    val source: Source[Int, NotUsed] = Source(1 to 10)
+    val result = source.runFold(0)(_ + _)
+    result map { r => assert(r == 10) }
   }
 }
