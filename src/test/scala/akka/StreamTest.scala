dfaeab11cfcde3e3c9b9b3a272c7448a864bf850
objektwerks <objektwerks@gmail.com>
2016-02-26 12:46:12 -0500

    refactored tests

--- src/test/scala/akka/StreamTest.scala
+++ src/test/scala/akka/StreamTest.scala
@@ -15,6 +15,7 @@ class StreamTest extends FunSuite with BeforeAndAfterAll {
   val source: Source[Int, NotUsed] = Source(1 to 10)
   val sink: Sink[Int, Future[Int]] = Sink.fold[Int, Int](0)(_ + _)
   val flow: Flow[Int, Int, NotUsed] = Flow.fromSinkAndSource(sink, source)
+  val graph: RunnableGraph[Future[Int]] = source.via(flow).toMat(sink)(Keep.right)
 
   override protected def afterAll(): Unit = {
     Await.result(system.terminate(), 1 second)
@@ -43,8 +44,7 @@ class StreamTest extends FunSuite with BeforeAndAfterAll {
   }
 
   test("graph") {
-    val graph = source.via(flow).toMat(sink)(Keep.right)
-    val result = graph.run()
-    result map { sum => assert(sum == 10) }
+    val future = graph.run()
+    future map { sum => assert(sum == 10) }
   }
 }
