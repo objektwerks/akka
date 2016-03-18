a7772e60c47ca1e89aa3bbd8fe551bc8a5bb70dd
objektwerks <objektwerks@gmail.com>
2016-03-18 11:11:32 -0400

    added supervision

--- src/test/scala/akka/StreamTest.scala
+++ src/test/scala/akka/StreamTest.scala
@@ -1,7 +1,8 @@
 package akka
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.Supervision.Decider
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 import akka.stream.scaladsl._
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
@@ -11,7 +12,8 @@ import scala.concurrent.{Await, Future}
 class StreamTest extends FunSuite with BeforeAndAfterAll {
   implicit val system: ActorSystem = ActorSystem.create("stream", Conf.config)
   implicit val ec = system.dispatcher
-  implicit val materializer = ActorMaterializer()
+  val decider: Decider = Supervision.resumingDecider
+  implicit val materializer = ActorMaterializer(ActorMaterializerSettings(system).withSupervisionStrategy(decider))
   val source: Source[Int, NotUsed] = Source(1 to 10)
   val sink: Sink[Int, Future[Int]] = Sink.fold[Int, Int](0)(_ + _)
   val flow: Flow[Int, Int, NotUsed] = Flow.fromSinkAndSource(sink, source)
