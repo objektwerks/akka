21be186a8bd8f4b3bbc4398050c8f81c9f6966eb
objektwerks <objektwerks@gmail.com>
2016-03-21 09:09:31 -0400

    renamed test.conf to test-akka.conf

--- src/test/scala/akka/Conf.scala
+++ src/test/scala/akka/Conf.scala
@@ -4,6 +4,6 @@ import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
 object Conf {
-  val config = ConfigFactory.load("test.conf")
+  val config = ConfigFactory.load("test-akka.conf")
   val log = LoggerFactory.getLogger(this.getClass)
 }
