db3e770fb86fae42b0c40a0a7df3b65d98f33a65
objektwerks <objektwerks@gmail.com>
2016-03-21 16:31:13 -0400

    removed log from conf

--- src/test/scala/akka/Conf.scala
+++ src/test/scala/akka/Conf.scala
@@ -1,9 +1,7 @@
 package akka
 
 import com.typesafe.config.ConfigFactory
-import org.slf4j.LoggerFactory
 
 object Conf {
   val config = ConfigFactory.load("test-akka.conf")
-  val log = LoggerFactory.getLogger(this.getClass)
 }
