2a25caa387c458dc6d68082074440262c5715bac
objektwerks <objektwerks@gmail.com>
2016-04-07 12:13:10 -0500

    added space

--- src/main/scala/cluster/SeedNode.scala
+++ src/main/scala/cluster/SeedNode.scala
@@ -9,7 +9,7 @@ import scala.concurrent.duration._
 
 class SeedNode(port: Int, role: String) {
   implicit val timeout = Timeout(10 seconds)
-  val conf = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$port").
+  val conf = ConfigFactory.parseString(s"akka.remote.netty.tcp.port = $port").
     withFallback(ConfigFactory.parseString(s"akka.cluster.roles = [$role]")).
     withFallback(ConfigFactory.load("seed-node-akka.conf"))
   val system = ActorSystem.create("brewery", conf)
