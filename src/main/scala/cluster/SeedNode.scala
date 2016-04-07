8328cfcbf94c75646d3b31f2622be4d5029e30fc
objektwerks <objektwerks@gmail.com>
2016-04-07 12:43:01 -0500

    added seed node app

--- src/main/scala/cluster/SeedNode.scala
+++ src/main/scala/cluster/SeedNode.scala
@@ -7,17 +7,18 @@ import com.typesafe.config.ConfigFactory
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class EmbeddedSeedNode(port: Int, role: String) {
+object SeedNode extends App {
   implicit val timeout = Timeout(10 seconds)
+  val port = args(0).toInt
+  val role = args(1)
   val conf = ConfigFactory.parseString(s"akka.remote.netty.tcp.port = $port").
     withFallback(ConfigFactory.parseString(s"akka.cluster.roles = [$role]")).
     withFallback(ConfigFactory.load("seed-node-akka.conf"))
   val system = ActorSystem.create(role, conf)
-  system.log.info(s"Embedded Seed Node initialized on port: $port for role: $role!")
-
-  def terminate(): Unit = {
+  system.log.info(s"Seed Node initialized on port: $port for role: $role!")
+  sys.addShutdownHook {
     implicit val ec = system.dispatcher
-    system.log.info(s"Embedded Seed Node on port: $port for role: $role terminating...")
+    system.log.info(s"Seed Node on port: $port for role: $role terminating...")
     Await.result(system.terminate(), 3 seconds)
   }
 }
