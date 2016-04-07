5cd3f5d19439bcb0aaf6412e1fc229ea4a482d8b
objektwerks <objektwerks@gmail.com>
2016-04-07 11:57:33 -0500

    refactored cluster event listening

--- src/main/scala/brewery/actor/Brewer.scala
+++ src/main/scala/brewery/actor/Brewer.scala
@@ -3,21 +3,11 @@ package brewery.actor
 import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.{Actor, ActorLogging, ActorRef}
-import akka.cluster.Cluster
-import akka.cluster.ClusterEvent.ClusterDomainEvent
 import brewery._
 
 class Brewer(masher: ActorRef) extends Actor with ActorLogging {
   val batchNumber = new AtomicInteger()
   val publisher = context.system.eventStream
-
-  override def preStart(): Unit = {
-    Cluster(context.system).subscribe(self, classOf[ClusterDomainEvent])
-  }
-
-  override def postStop(): Unit = {
-    Cluster(context.system).unsubscribe(self)
-  }
 
   override def receive: Receive = {
     case recipe: Recipe =>
@@ -27,6 +17,5 @@ class Brewer(masher: ActorRef) extends Actor with ActorLogging {
     case command: Command => Brewery.command(command)
     case state: State => Brewery.state(state)
     case event: Event => Brewery.event(event)
-    case clusterDomainEvent: ClusterDomainEvent => log.debug(s"Brewer: $clusterDomainEvent")
   }
 }
--- src/main/scala/cluster/Listener.scala
+++ src/main/scala/cluster/Listener.scala
@@ -1,9 +1,8 @@
 package cluster
 
 import akka.actor.{Actor, ActorLogging}
+import akka.cluster.Cluster
 import akka.cluster.ClusterEvent._
-import akka.cluster.{Cluster, MemberStatus}
-
 
 class Listener extends Actor with ActorLogging {
   override def preStart(): Unit = {
@@ -15,13 +14,7 @@ class Listener extends Actor with ActorLogging {
   }
 
   override def receive: Receive = {
-    case MemberUp(member) => log.info(s"*** Member Up Event: $member")
-    case MemberExited(member) => log.info(s"*** Member Exited: $member")
-    case MemberRemoved(member, previousState) =>
-      if(previousState == MemberStatus.Exiting) log.info(s"*** Member Removed: $member")
-      else log.info(s"*** Member Removed | Unreachable: $member")
-    case UnreachableMember(member) => log.info(s"*** Member Unreachable: $member")
-    case ReachableMember(member) => log.info(s"*** Member Reachable: $member")
-    case state: CurrentClusterState => log.info(s"*** Current Cluster State: $state")
+    case event: ClusterDomainEvent => log.info(s"$event")
+    case state: CurrentClusterState => log.info(s"$state")
   }
 }
