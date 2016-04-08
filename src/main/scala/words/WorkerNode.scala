c401bbf1a7034727fec71156d8b7f8361f5986d7
objektwerks <objektwerks@gmail.com>
2016-04-08 11:08:36 -0500

    removed worker actor create

--- src/main/scala/words/WorkerNode.scala
+++ src/main/scala/words/WorkerNode.scala
@@ -1,8 +1,5 @@
 package words
 
-import akka.actor.Props
 import cluster.Node
 
-object WorkerNode extends Node {
-  system.actorOf(Props[Worker], name = "worker")
-}
+object WorkerNode extends Node
