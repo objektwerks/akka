46138711b1c38747bc75c0100133768dcdd30356
objektwerks <objektwerks@gmail.com>
2016-04-08 17:59:26 -0500

    refactored master availability check

--- src/main/scala/words/Master.scala
+++ src/main/scala/words/Master.scala
@@ -8,7 +8,7 @@ class Master extends Actor with ActorLogging {
   val words = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\P{L}+")
 
   def receive = {
-    case workRequest: WorkRequest => sender ! CountWords(words)
+    case readyForWork: ReadyForWork => sender ! CountWords(words)
     case wordsCounted: WordsCounted => context.system.log.info(wordsCounted.toString)
   }
 }
--- src/main/scala/words/Work.scala
+++ src/main/scala/words/Work.scala
@@ -2,11 +2,16 @@ package words
 
 import java.time.LocalTime
 
-final case class WorkRequest(at: LocalTime = LocalTime.now)
+// State
+final case class ReadyForWork(at: LocalTime = LocalTime.now)
 
 sealed trait Work {
   def assigned: LocalTime = LocalTime.now
   def completed: Option[LocalTime]
 }
+
+// Command
 final case class CountWords(words: Array[String], completed: Option[LocalTime] = Some(LocalTime.now)) extends Work
+
+// Event
 final case class WordsCounted(counts: Map[String, Int], completed: Option[LocalTime] = Some(LocalTime.now)) extends Work
