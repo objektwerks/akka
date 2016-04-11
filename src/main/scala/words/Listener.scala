82a62cfef5f1ff1bc711240c59e0928684bab638
objektwerks <objektwerks@gmail.com>
2016-04-11 10:12:36 -0500

    added listener actor

---
+++ src/main/scala/words/Listener.scala
@@ -0,0 +1,35 @@
+package words
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent.MemberUp
+import akka.util.Timeout
+
+import scala.io.Source
+import scala.concurrent.duration._
+import scala.util.Random
+
+class Listener extends Actor with ActorLogging {
+  val words = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\P{L}+")
+  val (left, right) = words.splitAt(words.size / 2)
+  val cluster = Cluster(context.system)
+  val masters = IndexedSeq.empty[ActorRef]
+  val random = new Random
+
+  override def preStart(): Unit = cluster.subscribe(self, classOf[MemberUp])
+
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
+  implicit val ec = context.system.dispatcher
+  context.system.scheduler.schedule(2 seconds, 2 seconds) {
+    implicit val timeout = Timeout(3 seconds)
+    if (masters.nonEmpty) {
+      masters(random.nextInt(masters.length)) ! CountWords(left)
+      masters(random.nextInt(masters.length)) ! CountWords(right)
+    }
+  }
+
+  def receive = {
+    case MemberUp(member) if member.hasRole("master") => masters :+ member
+  }
+}
--- src/main/scala/words/MasterNode.scala
+++ src/main/scala/words/MasterNode.scala
@@ -1,22 +1,9 @@
 package words
 
 import akka.actor.Props
-import akka.util.Timeout
 import cluster.Node
 
-import scala.io.Source
-import scala.concurrent.duration._
-
 object MasterNode extends Node {
-  val words = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\P{L}+")
-  val (left, right) = words.splitAt(words.size / 2)
-
-  val master = system.actorOf(Props[Master], name = "master")
-
-  implicit val ec = system.dispatcher
-  system.scheduler.schedule(2 seconds, 2 seconds) {
-    implicit val timeout = Timeout(3 seconds)
-    master ! CountWords(left)
-    master ! CountWords(right)
-  }
+  system.actorOf(Props[Master], name = "master")
+  system.actorOf(Props[Listener], name = "listener")
 }
