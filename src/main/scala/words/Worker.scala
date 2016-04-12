e193e4fd2dcf5ab8a946435b8dc3a49310e4a384
objektwerks <objektwerks@gmail.com>
2016-04-12 08:18:16 -0500

    refactored worker to tract masters

--- src/main/scala/words/Worker.scala
+++ src/main/scala/words/Worker.scala
@@ -1,15 +1,15 @@
 package words
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
-import akka.cluster.Cluster
-import akka.cluster.ClusterEvent.MemberUp
+import akka.actor.{Actor, ActorLogging}
+import akka.cluster.{Cluster, Member, MemberStatus}
+import akka.cluster.ClusterEvent.{CurrentClusterState, MemberRemoved, MemberUp}
 
 import scala.collection.mutable
 import scala.util.Random
 
 class Worker extends Actor with ActorLogging {
   val cluster = Cluster(context.system)
-  val masters = mutable.ArraySeq.empty[ActorRef]
+  val masters = mutable.ArrayBuffer.empty[Member]
   val random = new Random
 
   override def preStart(): Unit = cluster.subscribe(self, classOf[MemberUp])
@@ -20,12 +20,30 @@ class Worker extends Actor with ActorLogging {
     case countWords: CountWords =>
       sender ! WordsCounted(countWords.id, toWordCount(countWords.words))
       readyForCommand()
+    case state: CurrentClusterState =>
+      state.members.filter(_.status == MemberStatus.Up).filter(_.hasRole("master")) foreach registerMember
     case MemberUp(member) if member.hasRole("master") =>
-      masters :+ member
+      registerMember(member)
+      readyForCommand()
+    case MemberRemoved(member, previousStatus) if member.hasRole("master") =>
+      unregisterMember(member)
       readyForCommand()
   }
 
-  private def readyForCommand(): Unit = masters(random.nextInt(masters.length)) ! ReadyForCommand
+  private def registerMember(member: Member): Unit = {
+    if (!masters.contains(member)) masters += member
+  }
+
+  private def unregisterMember(member: Member): Unit = {
+    if (!masters.contains(member)) masters -= member
+  }
+
+  private def readyForCommand(): Unit = {
+    if (masters.nonEmpty) {
+      val address = masters(random.nextInt(masters.length)).address.toString
+      context.actorSelection(address) ! ReadyForCommand
+    }
+  }
 
   private def toWordCount(words: Array[String]): Map[String, Int] = {
     words.groupBy((word: String) => word.toLowerCase).mapValues(_.length)
