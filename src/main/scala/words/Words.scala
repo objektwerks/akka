5f5020af183bea20350ef6fe4f45525cc049bb60
objektwerks <objektwerks@gmail.com>
2016-04-14 08:36:38 -0500

    added comment to words

--- src/main/scala/words/Words.scala
+++ src/main/scala/words/Words.scala
@@ -4,5 +4,5 @@ import scala.io.Source
 
 object Words {
   val list = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\P{L}+").toList
-  val words = list.grouped(21).toList
+  val words = list.grouped(21).toList // list.length = 168 / 21 = 8
 }
