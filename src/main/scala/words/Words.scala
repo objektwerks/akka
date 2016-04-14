1a089f6f5955407059d215082b198819553e4fd9
objektwerks <objektwerks@gmail.com>
2016-04-14 08:24:47 -0500

    refactored words

--- src/main/scala/words/Words.scala
+++ src/main/scala/words/Words.scala
@@ -3,11 +3,6 @@ package words
 import scala.io.Source
 
 object Words {
-  val words = Source
-    .fromInputStream(getClass.getResourceAsStream("/license.mit"))
-    .mkString
-    .split("\\P{L}+")
-    .toList
-    .sliding(21)
-    .toList
+  val list = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\P{L}+").toList
+  val words = list.grouped(21).toList
 }
