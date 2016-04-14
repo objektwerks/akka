3655818d7d3488e3ae539c45ffa03ccf447bdcd2
objektwerks <objektwerks@gmail.com>
2016-04-14 11:45:45 -0500

    added log statement to worker

--- src/main/scala/words/Worker.scala
+++ src/main/scala/words/Worker.scala
@@ -4,6 +4,8 @@ import akka.actor.{Actor, ActorLogging}
 
 class Worker extends Actor with ActorLogging {
   override def receive: Receive = {
-    case countWords: CountWords => sender ! WordsCounted(countWords, countWords.count)
+    case countWords: CountWords =>
+      log.info(s"Received $countWords from master: ${sender.path.name}")
+      sender ! WordsCounted(countWords, countWords.count)
   }
 }
