f0216578413e629030c60424d2227209d8990313
objektwerks <objektwerks@gmail.com>
2016-04-14 07:51:14 -0500

    reformatted words

--- src/main/scala/words/Words.scala
+++ src/main/scala/words/Words.scala
@@ -3,6 +3,11 @@ package words
 import scala.io.Source
 
 object Words {
-  val list = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\P{L}+").toList
-  val words = list.sliding(8).toList
+  val words = Source
+    .fromInputStream(getClass.getResourceAsStream("/license.mit"))
+    .mkString
+    .split("\\P{L}+")
+    .toList
+    .sliding(8)
+    .toList
 }
