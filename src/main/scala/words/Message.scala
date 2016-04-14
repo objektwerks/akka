00e61f6b12f6cfb6bc1fa099954ef8ba13638a67
objektwerks <objektwerks@gmail.com>
2016-04-14 09:44:34 -0500

    combined command and event into message

--- src/main/scala/words/Message.scala
+++ src/main/scala/words/Message.scala
@@ -3,16 +3,35 @@ package words
 import java.time.LocalDateTime
 import java.util.UUID
 
-sealed trait Message
-
-final case class Request(uuid: String = UUID.randomUUID.toString, words: List[List[String]] = Words.words) extends Message
+final case class Request(uuid: String = UUID.randomUUID.toString, words: List[List[String]] = Words.words)
 
 final case class ListOfCountWords(list: List[CountWords])
 
-final case class Response(uuid: String, assigned: LocalDateTime, completed: LocalDateTime, words: List[String], counts: Map[String, Int]) extends Message
+final case class Response(uuid: String, assigned: LocalDateTime, completed: LocalDateTime, words: List[String], counts: Map[String, Int])
 
 object Response {
   def apply(wordsCounted: WordsCounted): Response = {
     Response(wordsCounted.uuid, wordsCounted.assigned, wordsCounted.completed, wordsCounted.words, wordsCounted.counts)
+  }
+}
+
+sealed trait Command {
+  def uuid: String
+  def assigned: LocalDateTime = LocalDateTime.now
+}
+
+final case class CountWords(uuid: String, words: List[String]) extends Command
+
+sealed trait Event {
+  def uuid: String
+  def assigned: LocalDateTime
+  def completed: LocalDateTime = LocalDateTime.now
+}
+
+final case class WordsCounted(uuid: String, assigned: LocalDateTime, words: List[String], counts: Map[String, Int]) extends Event
+
+object WordsCounted {
+  def apply(countWords: CountWords, counts: Map[String, Int]): WordsCounted = {
+    WordsCounted(countWords.uuid, countWords.assigned, countWords.words, counts)
   }
 }
