fdf0816dba0fe4b1eb8213ec58db630f4746e77e
objektwerks <objektwerks@gmail.com>
2016-04-15 15:31:29 -0500

    added watch master to listener

--- src/main/scala/words/Listener.scala
+++ src/main/scala/words/Listener.scala
@@ -2,7 +2,7 @@ package words
 
 import java.util.concurrent.atomic.AtomicInteger
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, Props, Terminated}
 
 object Listener {
   private val masterNumber = new AtomicInteger()
@@ -17,6 +17,7 @@ class Listener extends Actor with ActorLogging {
       log.info(s"Listener received request from Client.")
       val master = context.actorOf(Props[Master], name = s"master-${Listener.nextMasterNumber}")
       log.info(s"Listener created Master [${master.path.name}].")
+      context.watch(master)
       val listOfCountWords = ListOfCountWords(request.words map { words => CountWords(request.uuid, words) })
       master ! listOfCountWords
     case wordsCounted: WordsCounted =>
@@ -26,6 +27,10 @@ class Listener extends Actor with ActorLogging {
     case fault: Fault =>
       log.error("Listener received fault, sent to Client and stopped Master!")
       publisher.publish(fault)
+      context.stop(sender)
+    case Terminated(master) =>
+      log.error("Listener received Master Terminated message, sent fault to Client, terminated Master!")
+      publisher.publish(Fault(s"Master [${master.path.name}] has been terminated"))
       context.stop(sender)
   }
 }
