db6c24d965ab6a6634ec6151daf89e7156ede1ae
objektwerks <objektwerks@gmail.com>
2016-04-15 16:36:47 -0500

    removed actor config and cluster listener debug

--- src/main/scala/cluster/ClusterListener.scala
+++ src/main/scala/cluster/ClusterListener.scala
@@ -17,6 +17,5 @@ class ClusterListener extends Actor with ActorLogging {
 
   override def receive: Receive = {
     case event: ClusterDomainEvent => log.info(s"$event")
-    case state: CurrentClusterState => log.info(s"$state")
   }
 }
