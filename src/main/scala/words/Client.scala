eab34ddc338c73be337d889cee172c0526e0c7ec
objektwerks <objektwerks@gmail.com>
2016-04-16 12:54:24 -0500

    removed log statements and move simulator from master node to client

--- src/main/scala/words/Client.scala
+++ src/main/scala/words/Client.scala
@@ -1,12 +1,17 @@
 package words
 
 import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.util.Timeout
+
+import scala.concurrent.duration._
 
 class Client(listener: ActorRef) extends Actor with ActorLogging {
+  implicit val ec = context.system.dispatcher
+  implicit val timeout = Timeout(60 seconds)
+  context.system.scheduler.scheduleOnce(30 seconds, listener, Request(words = Words.words))
+
   override def receive: Receive = {
-    case request: Request =>
-      log.info("Client received simulate drequest from MasterNode, sent to Listener.")
-      listener ! request
+    case request: Request => listener ! request
     case response: Response => log.info(s"Client received response: $response")
     case fault: Fault => log.error(s"Client received fault: $fault")
   }
--- src/main/scala/words/MasterNode.scala
+++ src/main/scala/words/MasterNode.scala
@@ -2,10 +2,7 @@ package words
 
 import akka.actor.Props
 import akka.cluster.Cluster
-import akka.util.Timeout
 import cluster.{EmbeddedSeedNode, Node}
-
-import scala.concurrent.duration._
 
 object MasterNode extends Node {
   val seedNode2551 = new EmbeddedSeedNode(conf = "words-seed-node.conf", port = 2551, actorSystem = "words")
@@ -18,10 +15,5 @@ object MasterNode extends Node {
     val client = system.actorOf(Props(new Client(listener)), name = "client")
     system.eventStream.subscribe(client, classOf[Response])
     system.eventStream.subscribe(client, classOf[Fault])
-    system.log.info("MasterNode up!")
-
-    implicit val ec = system.dispatcher
-    implicit val timeout = Timeout(60 seconds)
-    system.scheduler.scheduleOnce(30 seconds, listener, Request(words = Words.words))
   }
 }
