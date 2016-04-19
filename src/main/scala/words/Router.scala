9c9528e702131f9d82567196492e04db9bd15c99
objektwerks <objektwerks@gmail.com>
2016-04-19 14:39:41 -0500

    reformatted router

--- src/main/scala/words/Router.scala
+++ src/main/scala/words/Router.scala
@@ -7,7 +7,10 @@ import akka.routing.RoundRobinPool
 trait Router {
   this: Actor =>
 
-  private val settings = ClusterRouterPoolSettings(totalInstances = 4, maxInstancesPerNode = 2, allowLocalRoutees = false, useRole = Some("worker"))
+  private val settings = ClusterRouterPoolSettings(totalInstances = 4,
+                                                   maxInstancesPerNode = 2,
+                                                   allowLocalRoutees = false,
+                                                   useRole = Some("worker"))
   private val pool = RoundRobinPool(nrOfInstances = 4, supervisorStrategy = SupervisorStrategy.stoppingStrategy)
   val router = context.actorOf(ClusterRouterPool(pool, settings).props(Props[Worker]), name = Master.newRouterName)
 }
