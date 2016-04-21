aa7ea0710ddc13d05b05cee1db01b3bfccc06b08
objektwerks <objektwerks@gmail.com>
2016-04-21 12:34:54 -0500

    removed collector buffer with list

--- src/main/scala/words/Collector.scala
+++ src/main/scala/words/Collector.scala
@@ -1,15 +1,14 @@
 package words
 
-import scala.collection.mutable
 import scala.concurrent.duration.FiniteDuration
 
-class Collector[T](val timeout: FiniteDuration, val collect: Int, private val collection: mutable.Buffer[T]) {
+class Collector[T](val timeout: FiniteDuration, val collect: Int, private var collection: List[T]) {
   def add(item: T): this.type = {
-    collection += item
+    collection = item +: collection
     this
   }
 
-  def list: List[T] = collection.toList
+  def list: List[T] = collection
 
   def count: Int = collection.size
 
--- src/main/scala/words/Coordinator.scala
+++ src/main/scala/words/Coordinator.scala
@@ -3,13 +3,12 @@ package words
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import words.Master._
 
-import scala.collection.mutable
 import scala.concurrent.duration._
 
 class Coordinator(listener: ActorRef) extends Actor with ActorLogging {
   override def receive: Receive = {
     case words: Words =>
-      val collector = new Collector[Map[String, Int]](30 seconds, words.size, mutable.ArrayBuffer.empty[Map[String, Int]])
+      val collector = new Collector[Map[String, Int]](30 seconds, words.size, List.empty[Map[String, Int]])
       val master = context.actorOf(Props(new Master(self, collector)), name = newMasterName)
       master ! words
     case WordsCounted(count) =>
