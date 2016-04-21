2c02adb4457d9dc2e62217799ff7d750b73da03b
objektwerks <objektwerks@gmail.com>
2016-04-21 10:08:30 -0500

    added collector to master

--- src/main/scala/words/Master.scala
+++ src/main/scala/words/Master.scala
@@ -7,6 +7,15 @@ import akka.actor.{Actor, ActorLogging, ActorRef, ReceiveTimeout}
 import scala.collection.mutable
 import scala.concurrent.duration._
 
+class Collector[T](val collect: Int, val collection: mutable.ArrayBuffer[T]) {
+  def add(item: T): this.type = {
+    collection += item
+    this
+  }
+  def count: Int = collection.size
+  def isDone: Boolean = collect == collection.size
+}
+
 object Master {
   private val masterNumber = new AtomicInteger()
   private val routerNmumber = new AtomicInteger()
@@ -17,25 +26,23 @@ object Master {
 class Master(coordinator: ActorRef) extends Actor with Router with ActorLogging {
   implicit val ec = context.dispatcher
   val receiveTimeout = 30 seconds
-  val bufferedWordCounts = mutable.ArrayBuffer.empty[Map[String, Int]]
-  var requiredWordCounts = 0
+  var collector: Collector[Map[String, Int]] = _
 
   override def receive: Receive = {
     case words: Words =>
       context.setReceiveTimeout(receiveTimeout)
-      requiredWordCounts = words.size
+      collector = new Collector[Map[String, Int]](words.size, mutable.ArrayBuffer.empty[Map[String, Int]])
       words.list foreach {
         words => context.system.scheduler.scheduleOnce(100 millis, router, CountWords(words))
       }
     case WordsCounted(count) =>
-      bufferedWordCounts += count
-      if (bufferedWordCounts.size == requiredWordCounts) {
-        coordinator ! WordsCounted(WordsCounted.merge(bufferedWordCounts))
+      if (collector.add(count).isDone) {
+        coordinator ! WordsCounted(WordsCounted.merge(collector.collection))
       }
     case ReceiveTimeout =>
-      val partialCount = WordsCounted.merge(bufferedWordCounts)
+      val partialCount = WordsCounted.merge(collector.collection)
       val cause = s""""Master [${self.path.name}] timed out after ${receiveTimeout.toSeconds} seconds,
-                  completing $bufferedWordCounts.size of $requiredWordCounts word counts."""
+                  completing ${collector.count} of ${collector.collect} word counts."""
       coordinator ! PartialWordsCounted(partialCount, cause)
   }
 }
