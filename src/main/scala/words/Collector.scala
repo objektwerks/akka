217035c747cb2a687b0f0dcc39cd44fbc2d76891
objektwerks <objektwerks@gmail.com>
2016-04-21 10:28:36 -0500

    factored collector out of master; now coordinator creates and passes a collector to master

---
+++ src/main/scala/words/Collector.scala
@@ -0,0 +1,13 @@
+package words
+
+import scala.collection.mutable
+import scala.concurrent.duration.FiniteDuration
+
+class Collector[T](val timeout: FiniteDuration, val collect: Int, val collection: mutable.ArrayBuffer[T]) {
+  def add(item: T): this.type = {
+    collection += item
+    this
+  }
+  def count: Int = collection.size
+  def isDone: Boolean = collect == collection.size
+}
--- src/main/scala/words/Master.scala
+++ src/main/scala/words/Master.scala
@@ -4,17 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.{Actor, ActorLogging, ActorRef, ReceiveTimeout}
 
-import scala.collection.mutable
 import scala.concurrent.duration._
-
-class Collector[T](val collect: Int, val collection: mutable.ArrayBuffer[T]) {
-  def add(item: T): this.type = {
-    collection += item
-    this
-  }
-  def count: Int = collection.size
-  def isDone: Boolean = collect == collection.size
-}
 
 object Master {
   private val masterNumber = new AtomicInteger()
@@ -23,15 +13,12 @@ object Master {
   def newRouterName: String = s"router-${routerNmumber.incrementAndGet()}"
 }
 
-class Master(coordinator: ActorRef) extends Actor with Router with ActorLogging {
+class Master(coordinator: ActorRef, collector: Collector[Map[String, Int]]) extends Actor with Router with ActorLogging {
   implicit val ec = context.dispatcher
-  val receiveTimeout = 30 seconds
-  var collector: Collector[Map[String, Int]] = _
 
   override def receive: Receive = {
     case words: Words =>
-      context.setReceiveTimeout(receiveTimeout)
-      collector = new Collector[Map[String, Int]](words.size, mutable.ArrayBuffer.empty[Map[String, Int]])
+      context.setReceiveTimeout(collector.timeout)
       words.list foreach {
         words => context.system.scheduler.scheduleOnce(100 millis, router, CountWords(words))
       }
@@ -41,7 +28,7 @@ class Master(coordinator: ActorRef) extends Actor with Router with ActorLogging 
       }
     case ReceiveTimeout =>
       val partialCount = WordsCounted.merge(collector.collection)
-      val cause = s""""Master [${self.path.name}] timed out after ${receiveTimeout.toSeconds} seconds,
+      val cause = s""""Master [${self.path.name}] timed out after ${collector.timeout.toSeconds} seconds,
                   completing ${collector.count} of ${collector.collect} word counts."""
       coordinator ! PartialWordsCounted(partialCount, cause)
   }
