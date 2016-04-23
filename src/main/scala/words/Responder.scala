e732df669f3acb931fe2dc91a822c4e3a0535ea4
objektwerks <objektwerks@gmail.com>
2016-04-23 17:56:37 -0500

    edited log statements

--- src/main/scala/words/Responder.scala
+++ src/main/scala/words/Responder.scala
@@ -5,7 +5,7 @@ import akka.actor.{Actor, ActorLogging}
 
 class Responder extends Actor with ActorLogging {
   override def receive: Receive = {
-    case response: Response => log.info(s"Listener received response[${response.count.size}]: $response")
-    case response: PartialResponse => log.info(s"Listener received a partial response[${response.count.size}]: $response")
+    case response: Response => log.info(s"Responder received response[${response.count.size}]: $response")
+    case response: PartialResponse => log.info(s"Responder received a partial response[${response.count.size}]: $response")
   }
 }
