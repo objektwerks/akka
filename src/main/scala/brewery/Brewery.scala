ac0a74504ccea371dc7776ba5c5e7bb410078b9a
objektwerks <objektwerks@runbox.com>
2017-02-20 08:11:11 -0500

    renamed brewery event notification methods

--- src/main/scala/brewery/Brewery.scala
+++ src/main/scala/brewery/Brewery.scala
@@ -14,17 +14,18 @@ object Brewery {
   var commandPropertyListener: Option[ObjectProperty[Command]] = None
   var statePropertyListener: Option[ObjectProperty[State]] = None
   var eventPropertyListener: Option[ObjectProperty[Event]] = None
+
   val system = ActorSystem.create("brewery", ConfigFactory.load("brewery.conf"))
   val bottler = system.actorOf(Props[Bottler], name = "bottler")
   val kegger = system.actorOf(Props[Kegger], name = "kegger")
   val casker = system.actorOf(Props[Casker], name = "casker")
   val conditioner = system.actorOf(Props(classOf[Conditioner], bottler, kegger, casker), name = "conditioner")
   val fermenter = system.actorOf(Props(classOf[Fermenter], conditioner), name = "fermenter")
-
   val cooler = system.actorOf(Props(classOf[Cooler], fermenter), name = "cooler")
   val boiler = system.actorOf(Props(classOf[Boiler], cooler), name = "boiler")
   val masher = system.actorOf(Props(classOf[Masher], boiler), name = "masher")
   val brewer = system.actorOf(Props(classOf[Brewer], masher), name = "brewer")
+
   system.eventStream.subscribe(brewer, classOf[Command])
   system.eventStream.subscribe(brewer, classOf[State])
   system.eventStream.subscribe(brewer, classOf[Event])
@@ -40,11 +41,11 @@ object Brewery {
 
   def brew(recipe: Recipe): Unit = brewer ! recipe
 
-  def command(command: Command): Unit = commandPropertyListener foreach { _.value = command }
+  def onCommand(command: Command): Unit = commandPropertyListener foreach { _.value = command }
 
-  def state(state: State): Unit = statePropertyListener foreach { _.value = state }
+  def onState(state: State): Unit = statePropertyListener foreach { _.value = state }
 
-  def event(event: Event): Unit = eventPropertyListener foreach { _.value = event }
+  def onEvent(event: Event): Unit = eventPropertyListener foreach { _.value = event }
 
   def terminate(): Unit = {
     system.log.info("Brewery terminating...")
--- src/main/scala/brewery/actor/Brewer.scala
+++ src/main/scala/brewery/actor/Brewer.scala
@@ -14,8 +14,8 @@ class Brewer(masher: ActorRef) extends Actor with ActorLogging {
       val brew = Brew(batchNumber.incrementAndGet(), recipe)
       publisher.publish(brew)
       masher ! brew
-    case command: Command => Brewery.command(command)
-    case state: State => Brewery.state(state)
-    case event: Event => Brewery.event(event)
+    case command: Command => Brewery.onCommand(command)
+    case state: State => Brewery.onState(state)
+    case event: Event => Brewery.onEvent(event)
   }
 }
