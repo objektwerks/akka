59a1a873384dc8ee2186e72f5bfcced4107f59c6
objektwerks <objektwerks@gmail.com>
2019-06-16 07:50:42 -0400

    simplified deadletter test

--- src/test/scala/akka/DeadLetterTest.scala
+++ src/test/scala/akka/DeadLetterTest.scala
@@ -9,22 +9,22 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 
 class Service extends Actor with ActorLogging {
-  def receive = {
+  def receive: Receive = {
     case message: String => log.info(s"*** Service received: $message")
   }
 }
 
 class Listener extends Actor with ActorLogging {
-  def receive = {
+  def receive: Receive = {
     case _: DeadLetter => log.info(s"*** Listener received dead letter!")
   }
 }
 
 class DeadLetterTest extends FunSuite  with BeforeAndAfterAll {
   implicit val timeout = Timeout(1 second)
-  val system: ActorSystem = ActorSystem.create("deadletter", Conf.config)
-  val service: ActorRef = system.actorOf(Props[Service], name = "service")
-  val listener: ActorRef = system.actorOf(Props[Listener], name = "listener")
+  val system = ActorSystem.create("deadletter", Conf.config)
+  val service = system.actorOf(Props[Service], name = "service")
+  val listener = system.actorOf(Props[Listener], name = "listener")
   system.eventStream.subscribe(listener, classOf[DeadLetter])
 
   override protected def afterAll(): Unit = {
