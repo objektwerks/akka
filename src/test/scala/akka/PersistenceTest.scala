fc39738046e9edadb110d1f3b8533e2ea0802713
objektwerks <objektwerks@gmail.com>
2019-06-21 06:52:52 -0400

    refactored minor odds and ends in persistence test

--- src/test/scala/akka/PersistenceTest.scala
+++ src/test/scala/akka/PersistenceTest.scala
@@ -21,7 +21,7 @@ case class Computed(value: Int, created: LocalTime = LocalTime.now())
 
 case class Events(events: List[Computed] = Nil) {
   def add(event: Computed): Events = copy(event :: events)
-  def list: List[Int] = events.map(c => c.value)
+  def list: List[Computed] = events
 }
 
 case object Result
@@ -29,26 +29,26 @@ case object Snapshot
 case object Shutdown
 
 class Computer extends PersistentActor with ActorLogging {
+  var events = Events()
+
   override def persistenceId: String = "computer-persistence-id"
 
-  var state = Events()
-
   def updateState(event: Computed): Unit = {
-    state = state.add(event)
+    events = events.add(event)
   }
 
   override def receiveCommand: Receive = {
     case command: Compute => persistAsync(Computed(command.execute))(updateState)
-    case Snapshot => saveSnapshot(state)
+    case Snapshot => saveSnapshot(events)
     case SaveSnapshotSuccess(metadata) => log.info(s"*** Computer snapshot successful: $metadata")
     case SaveSnapshotFailure(_, reason) => throw reason
-    case Result => sender ! state.events
+    case Result => sender ! events.list
     case Shutdown => context.stop(self)
   }
 
   override def receiveRecover: Receive = {
-    case computed: Computed => updateState(computed)
-    case SnapshotOffer(_, snapshot: Events) => state = snapshot
+    case event: Computed => updateState(event)
+    case SnapshotOffer(_, snapshot: Events) => events = snapshot
     case RecoveryCompleted => log.info("*** Computer snapshot recovery completed.")
   }
 }
@@ -81,7 +81,7 @@ class PersistenceTest extends FunSuite with BeforeAndAfterAll {
     Thread.sleep(3000)
 
     val events = Await.result( (computer ? Result).mapTo[List[Computed]], 10 seconds)
-    println("fibonacci compute events:")
+    println("fibonacci computed events:")
     events.foreach(event => println(s"created: ${event.created} value: ${event.value}"))
     assert(events.size >= 10)
 
